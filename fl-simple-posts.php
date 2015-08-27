3d85af64347abc7881978ec5569f364e8fac745f
Martin Berggren <martin@mwi.se>
2015-08-27 08:57:57 +0200

    Bump version nr

--- fl-simple-posts.php
+++ fl-simple-posts.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Simple post module
  * Description: A plugin to list posts with a certain template
- * Version: 1.3
+ * Version: 1.3.1
  * Author: MWi AB
  * Author URI: http://mwi.se
  */
